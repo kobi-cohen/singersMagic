4e2028e8a774b39c6acdf180825df9b1302b8c31
kc <some@mail.com>
2022-10-29 10:57:02 +0300

    pictures added

--- display.js
+++ display.js
@@ -1,20 +1,29 @@
-const names = [
+const singers = [
 
-    {name:`מייקל ג'קסון`,born:1959},
-    {name:`שלמה ארצי`,born:1949},
-    {name:`אום כולתום`,born:1902},
-    {name:`מרגי`,born:2005},
-    {name:`נועה קירל`,born:2006},
-    {name:`דודו טסה`,born:1976},
+    {name:`מייקל ג'קסון`,born:1959,pic:'https://m.media-amazon.com/images/I/61xgsBsRjSL.jpg'},
+    {name:`שלמה ארצי`,born:1949,pic:'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,w_500/457526'},
+    {name:`ריטה`,born:1961,pic:'https://medias.atmag.co.il/www/uploads/2021/08/1630325779_15115-600x600-1630326381.jpg'},
+    {name:`מרגי`,born:2005,pic:'https://mhatzor.org.il/productImages2/44/2019/06/25/image1561453037.jpg'},
+    {name:`נועה קירל`,born:2006,pic:'https://www.euromix.co.il/wp-content/uploads/2022/07/Noa-Kirel-Israel-2023.jpg'},
+    {name:`דודו טסה`,born:1976,pic:'https://i1.sndcdn.com/artworks-000161780120-axcimh-t500x500.jpg'},
 ]
 
 const wrapper = document.querySelector('.wrapper')
 
-const popup = name =>{
-    console.log(name)
+const popup = singer =>{
     
     const display = document.querySelector('.display')
-    display.textContent=name.name+` ${name.born}`
+    display.textContent=singer.name+` ${singer.born}`
+    if (singer.pic){
+    const div = document.createElement('div')
+    const img = document.createElement('img')
+    img.style.width='100rem'
+    img.style.height='auto'
+    img.src=singer.pic
+    div.appendChild(img)
+    display.appendChild(div)
+    }
+
     display.classList.add('show')  
     display.addEventListener('click',()=>{
         display.classList.remove('show')
@@ -23,11 +32,11 @@ const popup = name =>{
 
 
 const createList = () =>{
-    names.map(name =>{
+    singers.map(singer =>{
         const div = document.createElement('div')
-        div.textContent = name.name
+        div.textContent = singer.name
         div.addEventListener('click',(e)=>{
-            popup(name)
+            popup(singer)
         })
         wrapper.appendChild(div)
     })
